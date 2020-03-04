af618c636dee6891259e38436f986e1701ee39e3
rustdm <34550563+rustdm@users.noreply.github.com>
2020-03-04 18:22:29 -0500

    reduce redundancies

--- buildSporthOSX.sh
+++ buildSporthOSX.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # if homebrew (The OSX package manager) is installed this command should work, else find out how to install homebrew
-sudo brew install libsndfile
+brew install libsndfile
 
-sudo git clone https://github.com/PaulBatchelor/Soundpipe 
+git clone https://github.com/PaulBatchelor/Soundpipe 
 cd Soundpipe/
-sudo make 
-sudo make install
+make 
+make install
 cd .. 
-sudo git clone https://github.com/PaulBatchelor/Sporth
+git clone https://github.com/PaulBatchelor/Sporth
 cd Sporth/
-sudo make 
-sudo make install 
+make 
+make install 
 
 
 # This command should generate a 5 second dialtone.wav file for the purpose of testing functionality. It will be saved in the Sporth directory.
